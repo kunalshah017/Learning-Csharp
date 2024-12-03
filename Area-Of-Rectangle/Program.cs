4bf1b755245ca954cc6409460f39d80924edd943
kunalshah017 <kunalmanishshah@gmail.com>
2024-12-03 21:19:32 +0530

    03-12-2024

---
+++ Area-Of-Rectangle/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RectangleArea
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length of the rectangle: ");
+            int length = int.Parse(Console.ReadLine());
+            Console.Write("Enter the width of the rectangle: ");
+            int width = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The area of the rectangle is: " + (length * width));
+        }
+    }
+
+}
