fe43dfdb16f8e8b2cd5e1bec3e6698986434ddc3
kunalshah017 <kunalmanishshah@gmail.com>
2024-12-17 16:45:09 +0530

    03-12-2024

---
+++ Console Programs/03-12-2024/Time-Mins-To-Hours-Mins/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TimeMinsToHoursMins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Minutes: ");
+            int min = int.Parse(Console.ReadLine());
+
+            int hours = Convert.ToInt32( min / 60 );
+
+            min = Convert.ToInt32( min % 60 );
+
+            Console.WriteLine( hours + "hrs : " + min +"mins");
+        }
+    }
+}
