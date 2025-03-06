6718179b823b68016b28664528204d3700b3e88d
admin <admin@31D-LAB6-11.SVV.local>
2025-03-06 12:55:40 +0530

    06-03-2025

---
+++ ASP.NET/Practical Programs/03-02-2025/ProductPriceCalculator/ProductPrice/Default.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ProductPrice.Default" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Product Price Calculator</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Product Price Calculator</h2>
+
+            <asp:Label ID="error" runat="server" Visible="false"></asp:Label>
+            
+            <!-- Product Dropdown -->
+            <asp:DropDownList ID="ddlProducts" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddlProducts_SelectedIndexChanged">
+                <asp:ListItem Text="Select a Product" Value="0"></asp:ListItem>
+                <asp:ListItem Text="Water Bottle" Value="50"></asp:ListItem>
+                <asp:ListItem Text="Book" Value="30"></asp:ListItem>
+                <asp:ListItem Text="Pens" Value="5"></asp:ListItem>
+            </asp:DropDownList>
+
+            <asp:Image ID="imgProduct" runat="server" Width="300px" />
+
+            <br /><br />
+
+            <asp:Label ID="lblFinalCost" runat="server" Text="Final Cost: $0.00"></asp:Label>
+
+            <br /><br />
+
+            <asp:TextBox ID="txtQuantity" runat="server"></asp:TextBox>
+
+            <asp:Button ID="btnCalculate" runat="server" Text="Calculate Total" OnClick="btnCalculate_Click" />
+
+            <br /><br />
+
+            <asp:Label ID="lblTotalPrice" runat="server" Text="Total Price: $0.00"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP.NET/Practical Programs/03-02-2025/ProductPriceCalculator/ProductPrice/Default.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProductPrice
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ddlProducts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ddlProducts.SelectedIndex == 0)
+            {
+                error.Text = "Please select a product.";
+                error.Visible = true;
+                imgProduct.ImageUrl = "";
+                return;
+            }
+            else if (ddlProducts.SelectedIndex == 1)
+            {
+                imgProduct.ImageUrl = "~/images/water_bottle.png";
+            }
+            else if (ddlProducts.SelectedIndex == 2)
+            {
+                imgProduct.ImageUrl = "~/images/books.jpg";
+            }
+            else if (ddlProducts.SelectedIndex == 3)
+            {
+                imgProduct.ImageUrl = "~/images/pens.jpg";
+            }
+
+            lblFinalCost.Text = "FInal Price: " + int.Parse(ddlProducts.SelectedValue) + "$";
+        }
+
+        protected void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtQuantity.Text)){
+                error.Text = "Enter Quantity";
+            }
+            else
+            {
+                lblTotalPrice.Text = "Total Price: " + int.Parse(ddlProducts.SelectedValue) * int.Parse(txtQuantity.Text) + "$"; 
+            }
+        }
+    }
+}
